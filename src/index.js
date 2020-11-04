3097a078db143e767f2701ffcf21f3278e7a224d
juaestiv <juaestiv@gmail.com>
2020-11-04 08:47:21 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import User from "./User";
+// Note: ensure you've installed axios with 'npm install axios'
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 'Outer' component that will contain all the User 'cards'
+class UserGrid extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = { users: [] };
+  }
+
+  // Runs when component is mounted
+  componentDidMount() {
+    // Get data for 50 users
+    axios
+      .get("https://randomuser.me/api/?results=50")
+      .then(response => {
+        // GET request was successful, store the users in state
+        this.setState({ users: response.data.results });
+      })
+      .catch(err => {
+        // GET failed, log the error
+        console.log(err);
+      });
+  }
+
+  render() {
+    const userList = this.state.users.map(u => (
+      <User
+        key={u.name.first}
+        name={u.name.first}
+        image={u.picture.medium}
+        quote={u.quote}
+      />
+    ));
+
+    return <div className="columns is-multiline">{userList}</div>;
+  }
+}
+
+ReactDOM.render(<UserGrid />, document.getElementById("root"));
